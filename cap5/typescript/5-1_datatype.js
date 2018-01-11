8d4b7247cc8923d265dadb853531a84f0254d988
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-01-11 17:13:45 +0100

    cap5 typescript array

--- cap5/typescript/5-1_datatype.js
+++ cap5/typescript/5-1_datatype.js
@@ -2,3 +2,9 @@ function diff(a) {
     return a - 2;
 }
 var aux = 9;
+var n = Math.abs(diff(aux));
+console.log(n);
+var p = ['Rossi', 'Ferrari', 'Bianchi'];
+var p2 = ['Rossi', 'Ferrari', 'Bianchi'];
+console.log(p);
+console.log(p2);
--- cap5/typescript/5-1_datatype.ts
+++ cap5/typescript/5-1_datatype.ts
@@ -2,3 +2,19 @@ function diff(a:any):number{
   return a-2;
 }
 let aux:number=9;
+
+let n = Math.abs(diff(aux));
+console.log(n);
+
+let p:string[] = ['Rossi','Ferrari','Bianchi'];
+let p2:Array<string>=['Rossi','Ferrari','Bianchi'];
+console.log(p);
+console.log(p2);
+
+/*
+Alessandros-MacBook-Pro:typescript netalex$ tsc 5-1_datatype.ts
+Alessandros-MacBook-Pro:typescript netalex$ node 5-1_datatype.js
+7
+[ 'Rossi', 'Ferrari', 'Bianchi' ]
+[ 'Rossi', 'Ferrari', 'Bianchi' ]
+*/
