37cd6c0f360c5d55aecd668e0860264dcbbc8137
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-01-11 17:19:03 +0100

    cap5 tuple

--- cap5/typescript/5-1_datatype.ts
+++ cap5/typescript/5-1_datatype.ts
@@ -11,10 +11,17 @@ let p2:Array<string>=['Rossi','Ferrari','Bianchi'];
 console.log(p);
 console.log(p2);
 
+//tuple (combined arrays)
+
+let p3:[number, string, number]=[3, "Rossi", 90, 2, 1, "Bianchi"];
+console.log(p3);
+
+
 /*
 Alessandros-MacBook-Pro:typescript netalex$ tsc 5-1_datatype.ts
 Alessandros-MacBook-Pro:typescript netalex$ node 5-1_datatype.js
 7
 [ 'Rossi', 'Ferrari', 'Bianchi' ]
 [ 'Rossi', 'Ferrari', 'Bianchi' ]
+[ 3, 'Rossi', 90, 2, 1, 'Bianchi' ]
 */
