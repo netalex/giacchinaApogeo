468a1e7b6a5e43df9a48f6297a9f856fd35773ff
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-01-11 16:24:17 +0100

    cap5 typescript error on compile time

---
+++ cap5/5-1_looselyTyped.js
@@ -0,0 +1,7 @@
+function diff(a){
+  return a-2;
+}
+
+let aux='s';
+let n = Math.abs(diff(aux));
+console.log(n);
---
+++ cap5/esempio.js
@@ -0,0 +1,6 @@
+function diff(a) {
+    return a - 2;
+}
+var aux = 's';
+var n = Math.abs(diff(aux));
+console.log(n);
