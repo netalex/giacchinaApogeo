71077ad7d9c774ece74aa327a830d7e07fb114c3
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-01-11 09:57:33 +0100

    cap4 classes 3 prototype

--- cap4/4-8_classEs5.js
+++ cap4/4-8_classEs5.js
@@ -9,8 +9,15 @@ console.log(obj);
 obj.empty();
 console.log(obj);
 
+closet.prototype.putShoes = function(num){
+  this.shoes=num;
+};
+
+obj.putShoes(90);
+console.log(obj);
 
 /*
 closet {shoes: 90, empty: ƒ}
-closet {shoes: 0, empty: ƒ}
+VM1644:10 closet {shoes: 0, empty: ƒ}
+VM1644:17 closet {shoes: 90, empty: ƒ}
 */
