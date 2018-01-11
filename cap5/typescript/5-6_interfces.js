4af1d81f2d346c663b12a84dbea43c697e29b15d
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-01-11 23:55:44 +0100

    cap5 typescript datatype interfaces 3 optional elements

--- cap5/typescript/5-6_interfces.js
+++ cap5/typescript/5-6_interfces.js
@@ -1,10 +1,17 @@
-var obj = { name: 'Mario', surname: 123 };
+var obj = { name: 'Mario', surname: 'brambilla' };
+var obj2 = { name: 'Madonna' };
 function helloWorld(obj) {
     console.log("ciao " + obj.name + " " + obj.surname);
 }
 helloWorld(obj);
+helloWorld(obj2);
 /*
 $ tsc 5-6_interfces.ts
 $ node 5-6_interfces.js
 ciao Mario Rossi
+
+$ tsc 5-6_interfces.ts
+5-6_interfces.ts(12,12): error TS2345: Argument of type '{ name: string; surname: number; }' is not assignable to parameter of type 'Hello'.
+  Types of property 'surname' are incompatible.
+    Type 'number' is not assignable to type 'string'.
 */
--- cap5/typescript/5-6_interfces.ts
+++ cap5/typescript/5-6_interfces.ts
@@ -1,16 +1,17 @@
 interface Hello {
   name:string;
-  surname:string;
+  surname?:string;
 }
 
-let obj = {name:'Mario', surname:123};
+let obj = {name:'Mario', surname:'brambilla'};
+let obj2 = {name:'Madonna'}
 
 function helloWorld(obj:Hello):void{
   console.log(`ciao ${obj.name} ${obj.surname}`);
 }
 
 helloWorld(obj);
-
+helloWorld(obj2);
 /*
 $ tsc 5-6_interfces.ts
 $ node 5-6_interfces.js
@@ -20,4 +21,8 @@ $ tsc 5-6_interfces.ts
 5-6_interfces.ts(12,12): error TS2345: Argument of type '{ name: string; surname: number; }' is not assignable to parameter of type 'Hello'.
   Types of property 'surname' are incompatible.
     Type 'number' is not assignable to type 'string'.
+
+    $ node 5-6_interfces.js
+    ciao Mario brambilla
+    ciao Madonna undefined
 */
