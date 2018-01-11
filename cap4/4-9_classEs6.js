cf7328c84b7c623bddbf19c87f695c43d6c10bda
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-01-11 11:02:16 +0100

    cap4 classes es6 prototype

--- cap4/4-9_classEs6.js
+++ cap4/4-9_classEs6.js
@@ -2,8 +2,15 @@ let closet = class {
   constructor(shoes){
     this.shoes=shoes;
   }
+  empty(){
+    this.shoes=0;
+  }
 };
 /*
-let pippo = new closet();
-pippo.shoes=90;
+let pippo=new closet(90)
+pippo.shoes
+> 90
+pippo.empty()
+pippo.shoes
+> 0
 */
