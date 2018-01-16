864d4aee9c31a4881c6e86b28313a3f5310296c2
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-01-16 22:49:20 +0100

    cap6 *ngFor

--- cap2/HelloW/src/app/dir-struct/dir-struct.component.ts
+++ cap2/HelloW/src/app/dir-struct/dir-struct.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dir-struct',
-  template: `<h2 *ngIf="title2">{{title2}}</h2>`,
+  templateUrl: './dir-struct.component.html',
   styleUrls: ['./dir-struct.component.css']
 })
 export class DirStructComponent  {
   title2:String;
+  array3:Array<any> = [{number:1},{number:2},{number:3},{number:4},{number:5}];
   constructor() {
     setTimeout(()=>{
       this.title2='Ciao Mondo';
